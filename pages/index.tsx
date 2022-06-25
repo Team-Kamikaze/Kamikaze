fc7a6d23827aa5cda6ab394c940a2246b8d680cd
Monique Chetty <55020016+MoniqueChetty@users.noreply.github.com>
2022-06-25 23:09:39 +0200

    Create Header, Top bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Topbar from "../components/Topbar";
 
 const Home: NextPage = () => {
+  const feedfilter = ["React", "NextJS", "CSS", "HTML", "PAPAREACT"];
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Kamikaze</title>
+        <link rel="icon" href="/images/KAMIKAZE icon.ico" />
       </Head>
       <Header />
-
+      <Topbar feedfilter={feedfilter} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
