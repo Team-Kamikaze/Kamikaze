e270f19f673369c4523857aa2de46488301720e4
mohamedsabrie <abosaadmohamed@gmail.com>
2022-06-25 18:36:16 +0200

    ...

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        {/* left side */}
+        {/* Middle side */}
+        {/* right side */}
+        
+    </header>
+  )
+}
+
+export default Header
